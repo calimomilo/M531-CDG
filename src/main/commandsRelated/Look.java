64f4985da4ae3de8b974503233b18d1099db64a4
DYLANeay <dylan.eray6@gmail.com>
2025-05-19 11:12:10 +0200

    Ajout de la commande Look

--- src/main/commandsRelated/Look.java
+++ src/main/commandsRelated/Look.java
@@ -1,5 +1,18 @@
 package main.commandsRelated;
 
-public class Look {
+/**
+ * @param Look class extends Command class and implements the execute method.
+ */
+
+public class Look extends Command{
+
+    public Look() {
+        super("look", "Look around the environment.");
+    }
+
+    @Override
+    public void execute(String[] args) {
+        System.out.println("You look around and see a beautiful landscape.");
+    }
 
 }
