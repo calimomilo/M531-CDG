eb719c28d481a890fde3884871bfc409e5da4450
Harpogma <gd.temp@pm.me>
2025-05-23 15:07:52 +0200

    Added getUserInput function

--- src/main/Game.java
+++ src/main/Game.java
@@ -1,5 +1,9 @@
 package main;
 
+import main.commandsRelated.CommandRegistry;
+
+import java.util.NoSuchElementException;
+import java.util.Scanner;
 
 public class Game {
 
@@ -12,9 +16,23 @@ public class Game {
         System.out.println("Running game...");
         // your runtime code here...
 
-
+        CommandRegistry cr = new CommandRegistry();
+        int i = 1;
+        do {
+            cr.parseCommandInput(getUserInput("What do you want to do?"));
+            i++;
+        } while (i < 3); //CHANGE THIS CONDITION !!!
 
         // end of game
+    }
+
+    public String getUserInput(String message) {
+        String userInput;
+        Scanner sc = new Scanner(System.in);
+        System.out.println(message);
+        userInput = sc.nextLine();
+        sc.close();
+        return userInput;
     }
 
 }
