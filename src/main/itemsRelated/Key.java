1d16402371295db5ee452c938b46261692b9f321
DYLANeay <dylan.eray6@gmail.com>
2025-05-28 11:27:35 +0200

    key class update

--- src/main/itemsRelated/Key.java
+++ src/main/itemsRelated/Key.java
@@ -1,9 +1,15 @@
 package main.itemsRelated;
 
+import main.Location;
+
 import main.Item;
 
 public class Key extends Item{
-    public Key(String name, String description) {
+
+    private Location location;
+
+    public Key(String name, String description, Location location) {
         super(name, description);
+        this.location = location;
     }
 }
