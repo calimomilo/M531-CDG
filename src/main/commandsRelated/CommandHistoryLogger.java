2a7e44ef58814e8e90076e8012fddcaf9d085eff
DYLANeay <dylan.eray6@gmail.com>
2025-06-04 11:33:52 +0200

    save working

--- src/main/commandsRelated/CommandHistoryLogger.java
+++ src/main/commandsRelated/CommandHistoryLogger.java
@@ -3,7 +3,7 @@ package main.commandsRelated;
 public class CommandHistoryLogger implements ICommandObserver {
     @Override
     public void onCommandExecuted(String command) {
-        // just log the command to the console
+        // Print the command to the console, totally removable if you dont like it but i found it funny and useful
         System.out.println("[History] Command executed: " + command);
     }
 }
